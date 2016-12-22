00f7a49d2ec4ca67e25c581624e627ae45257e20
Oskar Nyberg <oskar@oskarnyberg.com>
2016-12-22 23:28:13 +0100

    Added signals to client

--- poppin.lua
+++ poppin.lua
@@ -9,17 +9,38 @@ function poppin.init(name, command)
     prog.command = command
     poppin.apps[name] = prog
 
+    poppin.spawn(name, command)
+end
+
+function poppin.spawn(name, command)
     local rules = { floating = true }
     awful.spawn(command, rules, function(c) poppin.new(name, c) end)
-    naughty.notify({ title = "poppin!" })
 end
 
 function poppin.new(name, c)
     poppin.apps[name].client = c
+
+    c:connect_signal("unmanage", function()
+        poppin.apps[name].client = nil
+    end)
+
+    c:connect_signal("unfocus", function()
+        c.minimized = true
+    end)
 end
 
 function poppin.toggle(name)
-    poppin.apps[name].client.minimized = not poppin.apps[name].client.minimized
+    local app = poppin.apps[name]
+    local c = app.client
+    if c ~= nil then
+        c.minimized = not c.minimized
+        if not c.minimized then
+            -- TODO: Not working?
+            c:raise()
+        end
+    elseif app.command ~= nil then
+        poppin.spawn(name, app.command)
+    end
 end
 
 return poppin
